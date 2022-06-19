70c186863e387aa930553b5a3d395b8ebc51f5e6
jaronimoKL <JaronimoLOW@Mail.ru>
2022-06-19 23:49:37 +0500

    v1.10

--- config/settings_conf/local_settings.py
+++ config/settings_conf/local_settings.py
@@ -3,6 +3,7 @@ import os
 
 load_dotenv('./deployment/env/.env.local')
 
-SECRET_KEY = 'dw213r24rf24f'
-DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8o23rbt2_+&k3q+pmu)5%asj6y2w35gw345gw45gsdf12e123ejpkag')
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = ['*']
